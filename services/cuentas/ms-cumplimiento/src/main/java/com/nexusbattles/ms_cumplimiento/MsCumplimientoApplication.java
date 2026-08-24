971d055f75391c55c9784ebe4ba3c0fae4cff8d9
CristianCamiloChaparro <chaparrocamilo50@gmail.com>
2026-08-24 13:53:06 -0500

    cambios en la estructura de la carpeta cuentas, dentro de services

---
+++ services/cuentas/ms-cumplimiento/src/main/java/com/nexusbattles/ms_cumplimiento/MsCumplimientoApplication.java
@@ -0,0 +1,13 @@
+package com.nexusbattles.ms_cumplimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCumplimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCumplimientoApplication.class, args);
+	}
+
+}
---
+++ services/cuentas/ms-identidad/src/test/java/com/nexusbattles/ms_identidad/MsIdentidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusbattles.ms_identidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIdentidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
