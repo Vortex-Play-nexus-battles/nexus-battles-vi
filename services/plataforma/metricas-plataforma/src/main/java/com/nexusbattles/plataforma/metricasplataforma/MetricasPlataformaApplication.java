384aea255a77d854edddd4d64507d6b8a6b38a6f
Sgonzalez09 <santiago.gonzalezos@upb.edu.co>
2026-08-26 11:44:35 -0500

    feat(plataforma): implementacion base de 8 microservicios y orquestacion con docker compose

---
+++ services/plataforma/metricas-plataforma/src/main/java/com/nexusbattles/plataforma/metricasplataforma/MetricasPlataformaApplication.java
@@ -0,0 +1,11 @@
+package com.nexusbattles.plataforma.metricasplataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricasPlataformaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MetricasPlataformaApplication.class, args);
+    }
+}
